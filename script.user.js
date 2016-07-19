9acbbf4845b6e876e39f8a4197e659369df38760
Tom Corke <tom.corke@skyscanner.net>
2016-07-19 11:42:32 +0100

    Increase revision number

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skyscanner B2B Widget Injector
 // @namespace    http://tomcorke.com
-// @version      0.3.15
+// @version      0.3.16
 // @description  Test utility for Skyscanner B2B Widgets
 // @author       Tom Corke
 // @include      *
